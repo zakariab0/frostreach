72947c183b7c6b8e83e1b65f72da13283032d9ee
Zakaria Bounou <zakariabounouu@gmail.com>
2025-01-05 17:38:01 +0100

    fix

--- app.py
+++ app.py
@@ -41,5 +41,4 @@ def generate():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use PORT provided by Vercel
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
